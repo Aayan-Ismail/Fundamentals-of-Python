ccd1c219e51e3fcca880e7e0acd64063f4b9e71a
Aayan-Ismail <mohammed143ismail@gmail.com>
2025-06-28 17:58:28 +0100

    Add files via upload

---
+++ l3_while_loop.py
@@ -0,0 +1,25 @@
+import turtle
+pen = turtle. Turtle()
+paper = turtle. Screen()
+
+# for i in range(36):
+#     print("i")
+
+# WHILE LOOP
+# i = 0
+# while i <36:
+#     print(i)
+#     i = i + 1 
+
+# i = 1
+# while i <16:
+#     print(i)
+#     i = i + 1
+
+i = 0
+while i < 8:
+    pen.forward(50)
+    pen.right(45)
+
+    i = i + 1
+turtle.done()
