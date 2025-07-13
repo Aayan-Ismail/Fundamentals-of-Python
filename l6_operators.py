da7221e5c4ec49efdf1784bb0bf6aa7c1298fe57
Aayan-Ismail <mohammed143ismail@gmail.com>
2025-07-13 18:01:46 +0100

    Add files via upload

---
+++ l6_operators.py
@@ -0,0 +1,20 @@
+#logical arithmatic comparison, assign
+
+# x = int(input("enter your first value ")) 
+# y = int(input("enter your second value "))
+
+# print("the sum is ", x + y)
+# print("the difference is ", x - y)
+# print(" the division is ", x / y)
+# print(" the floor division is ", x // y)
+# print("the exponent is ", x ** y)
+# print("the multiplication is ", x * y)
+# print("the remainder is ", x % y)
+
+things_you_buy = input("which pen would you prefer? ")
+print("i want " + things_you_buy)
+number = int(input("how many pens would you like? "))
+print("i bought ", number, "pens " )
+cost = 5
+bill = cost * number
+print("The price is ", bill, "euros ")
