0b849742e9f7837fce1c40e961ce6acbfa831b23
Aayan-Ismail <mohammed143ismail@gmail.com>
2025-07-19 17:59:22 +0100

    Add files via upload

---
+++ student_marks.py
@@ -0,0 +1,22 @@
+marks = int(input("how many marks did you get out of 100? "))
+i = 0
+
+if marks>=90 and marks<=100:
+    print("A")
+
+elif marks>=75 and marks<=89:
+    print("B")
+
+elif marks>=60 and marks<=74:
+    print("C")
+
+elif marks>=45 and marks<=59:
+    print("D")
+
+elif marks>=0 and marks<=44:
+    print("Fail")
+
+elif marks>=100 and marks<=10000000000000000000000000000000000000000000000000000000000000:
+    print("sure buddy")
+
+
