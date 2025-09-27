f13049e01792b4a1015008dbb0594be60ae15911
Aayan-Ismail <mohammed143ismail@gmail.com>
2025-09-27 17:53:54 +0100

    Add files via upload

---
+++ patterns.py
@@ -0,0 +1,10 @@
+# for i in range(65,70):
+#     for j in range(65,i + 1):
+#         print(chr(j),end = " ")
+#     print()
+
+for i in range(11,1,-1):
+    for j in range(1,i + 0):
+        print(j,end = " ")
+    print()
+    
